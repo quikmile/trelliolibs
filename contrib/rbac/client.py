013ead2122dd7a2c3d6ad03b6ae8ec08537249b3
nirmal@virtualbox <nirmal@artifilabs.com>
2017-01-03 11:49:51 +0000

    rbac client updated for other services

--- contrib/rbac/client.py
+++ contrib/rbac/client.py
@@ -8,7 +8,15 @@ class RBACTCPClient(TCPServiceClient):
     @request
     def verify_access(self, user_id, resource, resource_action):
         return locals() #@request requires a dict containing params describing the request payload
+    #
+    # @subscribe
+    # def user_permissions_updated(self, *args, **kwargs):
+    #     pass
 
-    @subscribe
-    def user_permissions_updated(self, *args, **kwargs):
-        pass
+    @request
+    def get_user_permissions(self, user_id):
+        return locals()
+
+    @request
+    def create_service_resources(self, service_name, service_version,http_resources):
+        return locals()
