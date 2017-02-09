de84964b712b8d22e3928c7773b86465b17e7c9b
Nirmal Singh <niks660097@gmail.com>
2017-02-09 14:10:32 +0530

    rbac client removed.

--- trelliolibs/__init__.py
+++ trelliolibs/__init__.py
@@ -1,6 +1,3 @@
-from .contrib.rbac.client import *
-from .contrib.rbac.exceptions import *
-from .contrib.rbac.middlewares import *
 from .utils.helpers import *
 
-__all__ = ['RBACTCPClient', 'AccessError', 'VerifyUserAccess', 'json_response', 'json_file_to_dict', 'RecordHelper']
+__all__ = ['json_response', 'json_file_to_dict', 'RecordHelper']
