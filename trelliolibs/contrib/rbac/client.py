4519f07aacc6943e1bb73f697929e858d23416b1
nirmal_artifici <nirmal@artifici.com>
2017-02-17 16:04:56 +0530

    rbac auto create resources done

--- trelliolibs/contrib/rbac/client.py
+++ trelliolibs/contrib/rbac/client.py
@@ -3,14 +3,11 @@ from trellio.services import TCPServiceClient, request
 
 class RBACTCPClient(TCPServiceClient):
     def __init__(self, *args, **kwargs):
-        super(RBACTCPClient, self).__init__("RBACTcpService", 1)
+        super(RBACTCPClient, self).__init__("rbac_service", 1)
+
 
     @request
-    def verify_access(self, user_id, resource, resource_action):
-        return locals()  # @request requires a dict containing params describing the request payload
-
-    @request
-    def get_user_permissions(self, user_id):
+    def create_resource_action(self, resource_id, action_name, action_value):
         return locals()
 
     @request
--- trelliolibs/contrib/rbac/utils.py
+++ trelliolibs/contrib/rbac/utils.py
@@ -1,25 +1,35 @@
-from trelliolibs.contrib.rbac import get_current_rbac_service_details
+import inspect
+import logging
+import json
+from trellio.host import Host
+from trellio.signals import register, ServiceReady
 
-class RbacTcpClientMissing(Exception):
-	pass
+def activate_auto_create_resources():
+    from trelliolibs.contrib.rbac.client import RBACTCPClient
+    @register(ServiceReady)
+    async def register_service_resources():
+        _logger = logging.getLogger(__name__)
+        http_service = Host._http_service
+        service_name = http_service.name
+        service_version = http_service.version
+        rbac_client = RBACTCPClient()
+        api_methods = []
+        exclude_resources = ['/ping', '/_stats']
+        for i in dir(http_service):
+            attr = getattr(http_service,i)
+            if inspect.ismethod(attr) and getattr(attr, 'is_http_method', None):
+                api_methods.append(attr)
+        resource_dict = {}
+        for i in api_methods:
+            path = i.paths[0]
+            method = i.method
+            if path not in exclude_resources:
+                if resource_dict.get(path):
+                    resource_dict[path].append(method)
+                else:
+                    resource_dict[path] = [method]
+        result = await rbac_client.create_service_resources(service_name, service_version,http_resources=resource_dict)
+        _logger.info('RBAC auto-create service resources response %s' %str(json.dumps(result)))
 
-async def register_service_resources(host_class):
-	tcp_clients = host_class.get_tcp_clients()
-	service_details = get_current_rbac_service_details()
-	rbac_client = None
-	for i in tcp_clients:
-		if (i.name,i.version) = service_details.TCP:
-			rbac_client = i
-	if not rbac_client:
-		raise RbacTcpClientMissing()
 
-	http_service = host_class.get_http_service()
-	resource_kwargs = {'service_name':http_service.name, 'service_version':http_service.version, 'http_resources':[]}
-	for i in dir(http_service):
-		_res = getattr(http_service, i, None)
-		if _res:
-			if getattr(_res, 'is_http_method', None):
-				_res = _res.paths #trellio version dependendant code
-				resource_kwargs['http_resources'].extend(_res)
 
-	await rbac_client.create_service_resources(**resource_kwargs)
