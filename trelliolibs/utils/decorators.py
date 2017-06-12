16d74dc19bb376c8e368a5a1e3085d5dd50befdd
Abhishek Verma <ashuverma1989@gmail.com>
2017-06-12 12:58:31 +0530

    added validate_schema method for json payload validation

--- trelliolibs/utils/decorators.py
+++ trelliolibs/utils/decorators.py
@@ -1,8 +1,37 @@
 from functools import wraps
+from uuid import UUID
 
+from cerberus import Validator
 from trellio import HTTPService, TCPService
 
 from .helpers import json_response
+
+
+class TrellioValidator(Validator):
+    def _validate_type_uuid(self, value):
+        if isinstance(value, UUID):
+            return True
+
+
+def validate_schema(schema=None, allow_unknown=False):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(self, *args, **kwargs):
+            if schema:
+                v = TrellioValidator(schema, allow_unknown=allow_unknown)
+                if isinstance(self, HTTPService):
+                    request = args[0]
+                    payload = await request.json()
+                    if not v.validate(payload):
+                        return json_response({'error': v.errors})
+                elif isinstance(self, TCPService):
+                    if not v.validate(kwargs):
+                        return {'error': v.errors}
+            return await func(self, *args, **kwargs)
+
+        return wrapper
+
+    return decorator
 
 
 def required_params(*params):
