b43d26574cfdf44200bb94b6a84047d836d0e131
abhishek <ashuverma1989@gmail.com>
2017-07-12 23:42:42 +0530

    HTTPView & TCPView support added to validate_schema

--- trelliolibs/utils/decorators.py
+++ trelliolibs/utils/decorators.py
@@ -2,7 +2,7 @@ from functools import wraps
 from uuid import UUID
 
 from cerberus import Validator
-from trellio import HTTPService, TCPService
+from trellio import HTTPService, TCPService, HTTPView, TCPView
 
 from .helpers import json_response
 
@@ -19,12 +19,12 @@ def validate_schema(schema=None, allow_unknown=False):
         async def wrapper(self, *args, **kwargs):
             if schema:
                 v = TrellioValidator(schema, allow_unknown=allow_unknown)
-                if isinstance(self, HTTPService):
+                if isinstance(self, HTTPService) or isinstance(self, HTTPView):
                     request = args[0]
                     payload = await request.json()
                     if not v.validate(payload):
                         return json_response({'error': v.errors}, status=400)
-                elif isinstance(self, TCPService):
+                elif isinstance(self, TCPService) or isinstance(self, TCPView):
                     if not v.validate(kwargs):
                         return {'error': v.errors}
             return await func(self, *args, **kwargs)
@@ -38,13 +38,13 @@ def required_params(*params):
     def decorator(func):
         @wraps(func)
         async def wrapper(self, *args, **kwargs):
-            if isinstance(self, HTTPService):
+            if isinstance(self, HTTPService) or isinstance(self, HTTPView):
                 request = args[0]
                 payload = await request.json()
                 missing_params = list(filter(lambda x: x not in payload.keys(), params))
                 if missing_params:
                     return json_response({'error': 'required params - {} not found'.format(', '.join(missing_params))})
-            elif isinstance(self, TCPService):
+            elif isinstance(self, TCPService) or isinstance(self, TCPView):
                 missing_params = list(filter(lambda x: x not in kwargs.keys(), params))
                 if missing_params:
                     return {'error': 'required params - {} not found'.format(', '.join(missing_params))}
