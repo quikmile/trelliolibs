e51d0d95ea0ef62fb3fedd8b6dc2dce27924c07d
abhishek <ashuverma1989@gmail.com>
2017-08-27 18:38:01 +0530

    fxied pagination

--- trelliolibs/utils/base.py
+++ trelliolibs/utils/base.py
@@ -151,7 +151,7 @@ class CRUDModel(BaseSignal):
         result = await self._db.update(table=self._table, where_dict=where_dict, **values)
         return self._record.record_to_dict(result, normalize=self._serializers)
 
-    async def paginate(self, limit=10, offset=0, order_by='created desc', **filter) -> dict:
+    async def paginate(self, limit: int = 10, offset: int = 0, order_by: str = 'created desc', **filter) -> dict:
         coros = [self.filter(limit=limit, offset=offset, order_by=order_by, **filter), self.count(**filter)]
         records, count = await gather(*coros, return_exceptions=True)
 
