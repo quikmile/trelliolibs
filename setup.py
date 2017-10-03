1f864ae597175fefdc0b5ddddc5d6b4094a07f52
abhishek <ashuverma1989@gmail.com>
2017-10-03 19:52:00 +0530

    rbac activate added

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='trelliolibs',
-      version='0.36.6',
+      version='0.36.7',
       author='Abhishek Verma, Nirmal Singh',
       author_email='ashuverma1989@gmail.com, nirmal.singh.cer08@itbhu.ac.in',
       url='https://github.com/technomaniac/trelliolibs',
